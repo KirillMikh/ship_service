6e5cc5137c1cb4668aef3389ee1d485291a4d719
Admin <kirill.mikhalev.9035@gmail.com>
2020-05-17 21:43:47 +0300

    5

---
+++ libs/task_generator.py
@@ -0,0 +1,54 @@
+from libs.postres_db import DbPg
+from libs.rabbit_wrapper import RabbitQueue
+
+CRAWLER_QUEUE_NAME = "crawl_pages"
+CRAWLER_EXCHANGE_NAME = "ex_crawl_pages"
+MAX_QUEUE_SIZE = 100
+PAGE_URL = "https://www.vesselfinder.com/vessels?page={num}"
+MAX_PAGES = 23_867
+class ShipsGenerator:
+    def __init__(self, exit_event):
+        self.exit_event = exit_event
+
+        self.was_pages = {}
+
+        self.db = DbPg(logger=None)
+        self.rqueue = RabbitQueue(CRAWLER_EXCHANGE_NAME, CRAWLER_QUEUE_NAME)
+
+        self.wait_queue()
+        self.init_progress_table()
+        self.get_ready_tasks()
+
+    def wait_queue(self):
+        while self.rqueue.count() > 0:
+
+            if self.exit_event.wait(10):
+                break
+
+    def get_ready_tasks(self):
+        query = '''SELECT * FROM pages'''
+        for row in self.db.get_query(query):
+            self.was_pages[row[0]] = True
+
+
+    def run(self):
+        for i in range(MAX_PAGES):
+            if self.exit_event.is_set():
+                break
+
+            if self.was_pages.get(i):
+                continue
+
+            msg = {'url': PAGE_URL.format(num=i),
+                   'num': i}
+
+
+            while self.rqueue.count() > MAX_QUEUE_SIZE:
+                if self.exit_event.wait(5):
+                    return
+            self.rqueue.publish(msg)
+
+
+    def init_progress_table(self):
+        query = '''CREATE TABLE IF NOT EXISTS pages (page_num integer UNIQUE )'''
+        self.db.exec_query(query)
